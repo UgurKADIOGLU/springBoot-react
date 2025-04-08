e98bbc0b81b5cf44b4889bad4af1c025ddfd0c31
Uğur KADIOĞLU <ugrkadioglu@gmail.com>
2025-04-08 12:04:58 +0300

    Calışan crud işlemleri yapıldı

--- ws/src/main/java/com/isg/ws/calisan/i/ICalisanRepository.java
+++
@@ -1,8 +0,0 @@
-package com.isg.ws.calisan.i;
-
-import com.isg.ws.calisan.Calisan;
-import com.isg.ws.calisan.dto.DtoCalisan;
-
-public interface ICalisanRepository {
-    public DtoCalisan save(DtoCalisan dtoCalisan);
-}
